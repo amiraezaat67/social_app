7ae50f471c0569ec6c7d189c43cb64417c1a6a5b
amiraezaat67 <amira.ezaat@shezlong.com>
2025-02-09 19:49:45 +0200

    update

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const database_connection = async () => {
   try {    
-    await mongoose.connect(process.env.DB_URL)
+    await mongoose.connect(process.env.DB_URL,{
+      tls: true  // Enables SSL connection
+    })
+    
     console.log('Database connected');
   } catch (error) {
     console.log('Database not connected' ,  error);
